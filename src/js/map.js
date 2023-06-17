b26a29389c2b36ca6ad4f05656bfe9f6bd1ca8ca
lulujal <jaluadhi19@students.unnes.ac.id>
2023-06-18 02:13:39 +0700

    commit pertama

--- src/js/map.js
+++ src/js/map.js
@@ -1,13 +1,15 @@
-function initMap() {
-    const unnes = {lat: -7.0697, lng: 110.8608};
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 16,
-        center: unnes
-    });
-}
-    const marker = new google.maps.Marker({
-        position: unnes,
-        map: map,
-    });
+/** @format */
 
+function initMap() {
+  const unnes = { lat: -7.0505452, lng: 110.3924254 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 37,
+    center: unnes,
+  });
+
+  const marker = new google.maps.Marker({
+    position: unnes,
+    map: map,
+  });
+}
 window.initMap = initMap;
